9f3f6651b1a962abdd1bf3b9adc2b36373ed285b
KHANRAA <khanraakash@gmail.com>
2018-05-05 23:22:50 +0530

    day1

---
+++ zombie.c
@@ -0,0 +1,12 @@
+//
+// Created by root on 5/5/18.
+//
+
+#include "zombie.h"
+
+int main()
+{
+
+
+
+}
---
+++ zombie.h
@@ -0,0 +1,15 @@
+//
+// Created by root on 5/5/18.
+//
+
+#ifndef UNTITLED3_ZOMBIE_H
+#define UNTITLED3_ZOMBIE_H
+
+#endif //UNTITLED3_ZOMBIE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
